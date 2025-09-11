c409ec2a1566621d04fbb20b98867fb000e970e6
Tim Guttzeit <t.guttzeit@tuxedocomputers.com>
2025-09-11 14:43:27 -0600

    drm/i915/display: Avoid unsupported 300Hz output mode on a TUXEDO device
    Removes all display modes with more than 240 Hz for the integrated display
    on a TUXEDO Stellaris 16 Gen7, because using the iGPU with higer refresh
    rates causes screen flicker.
    
    Signed-off-by: Tim Guttzeit <t.guttzeit@tuxedocomputers.com>
    Co-developed-by: Werner Sembach <wse@tuxedocomputers.com>
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>
    Link: https://gitlab.freedesktop.org/drm/i915/kernel/-/issues/14616
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- drivers/gpu/drm/i915/display/intel_quirks.h
+++ drivers/gpu/drm/i915/display/intel_quirks.h
@@ -21,6 +21,7 @@ enum intel_quirk_id {
 	QUIRK_NO_PPS_BACKLIGHT_POWER_HOOK,
 	QUIRK_FW_SYNC_LEN,
 	QUIRK_EDP_LIMIT_RATE_HBR2,
+	QUIRK_EDP_MAX_240HZ_HOOK,
 };
 
 void intel_init_quirks(struct intel_display *display);
